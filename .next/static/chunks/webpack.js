a6ddf31a2734ebc4e0d8095b183670f357d081df
Toni Verger <toniverger@msn.com>
2020-11-26 09:50:20 -0300

    day 1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f7ebe8d2312dcf5cbd01";
+/******/ 	var hotCurrentHash = "59059b056fc36ec76c49";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,6 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-    
-  res.status(200).json({ name: 'John Doe' });
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function getPosts(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await prisma.post.create({
+    data: { title: "My post title", body: "This is my amazing post!" },
+  });
+
+  res.status(200).json({ name: "John Doe" });
 }
